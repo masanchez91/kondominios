c42895c8d21de43f876c923a145b88a71469b7e0
Mauro Sánchez <mauro.sanchez.simental@gmail.com>
2024-05-07 22:54:07 -0600

    :art: feat: agregar eslint y husky

--- src/routes/Private.tsx
+++ src/routes/Private.tsx
@@ -3,15 +3,17 @@ import { Navigate, Route } from "react-router-dom";
 import { PrivateRoutes } from "../models";
 import { RoutesWithNotFound } from "../utilities";
 
-const Dashboard = lazy(() => import("../pages/Private/Dashboard/DashboardPageView"));
+const Dashboard = lazy(
+  () => import("../pages/Private/Dashboard/DashboardPageView"),
+);
 const Clients = lazy(() => import("../pages/Private/Clients/ClientsPageView"));
 
 function Private() {
   return (
     <RoutesWithNotFound>
-		<Route path="/" element={<Navigate to={PrivateRoutes.DASHBOARD} />} />
-		<Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
-		<Route path={PrivateRoutes.CLIENTS} element={<Clients />} />
+      <Route path="/" element={<Navigate to={PrivateRoutes.DASHBOARD} />} />
+      <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
+      <Route path={PrivateRoutes.CLIENTS} element={<Clients />} />
     </RoutesWithNotFound>
   );
 }
